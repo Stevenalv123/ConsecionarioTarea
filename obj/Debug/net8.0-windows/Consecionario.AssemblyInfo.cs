f5f51d79acc397ceb30cf9db3be610389b39ffcc
Stevenalv123 <stevenalv222@gmail.com>
2024-10-19 14:34:34 -0600

    segundo

--- obj/Debug/net8.0-windows/Consecionario.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Consecionario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consecionario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda240971307c5bb857ac4450df5a382e36fcf3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consecionario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consecionario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
